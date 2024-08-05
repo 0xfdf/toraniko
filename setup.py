253ce7044ef33eb718e671e9f918a88ba5fa3350
fdf <0xfdf@implies.com>
2024-08-05 00:58:20 -0400

    Update setup.py
    make sure requirements are in setup

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup, find_packages
 
+requirements = []
+with open('requirements.txt', 'r') as fh:
+    for line in fh:
+        requirements.append(line.strip())
 
 setup(
     name="toraniko",
     version="1.1.0",
     packages=find_packages(),
+    install_requires = requirements
 )
