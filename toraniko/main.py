65f1cb1cddd151f58da90dd4d5387f016ce4f48f
0xfdf <0xfdf@implies.com>
2024-08-11 17:37:05 -0400

    full documentation of config options

--- toraniko/main.py
+++ toraniko/main.py
@@ -1,17 +1,26 @@
 """End-to-end estimation of the style factor scores, factor returns and factor covariance."""
 
 import configparser
+from io import StringIO
 import logging
-
 from typing import Literal
+
+import polars as pl
+
+from toraniko.utils import create_dummy_variables
 
 
 class FactorModel:
 
-    def __init__(self, config_file: str, log_level: Literal["info", "error", "debug"] = "info") -> None:
+    def __repr__(self):
+        return f"<Toraniko.FactorModel: {self.str_config}>"
+
+    def __init__(self, config_file: str, log_level: Literal["info", "error", "debug"] = "info"):
         """"""
         self.config = configparser.ConfigParser()
         self.config.read(config_file)
+        self.str_config = StringIO()
+        self.config.write(self.str_config)
         self.logger = logging.getLogger(__name__)
         self.logger.setLevel(log_level)
 
@@ -24,5 +33,5 @@ class FactorModel:
     def _estimate_factor_cov(self) -> None:
         """"""
 
-    def estimate(self) -> None:
+    def estimate(self, sector_scores: pl.DataFrame) -> None:
         """"""
